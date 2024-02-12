3840b9476936b5218f56f05269d6501b417a7a74
KonnorR <konnor0601@gmail.com>
2024-02-12 11:37:30 -0500

    New

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -79,6 +79,9 @@ public class SwerveSubsystem extends SubsystemBase
     speedValue.setDefaultOption("4", 14.5);
 
     SmartDashboard.putData(speedValue);
+
+    maximumSpeed = speedValue.getSelected();
+
     // Angle conversion factor is 360 / (GEAR RATIO * ENCODER RESOLUTION)
     //  In this case the gear ratio is 16.8 motor revolutions per wheel rotation.
     //  The encoder resolution per motor revolution is 1 per motor revolution.
@@ -309,8 +312,6 @@ public class SwerveSubsystem extends SubsystemBase
   }
 
   public void teleopInit() {
-
-    maximumSpeed = speedValue.getSelected();
 
   }
 
